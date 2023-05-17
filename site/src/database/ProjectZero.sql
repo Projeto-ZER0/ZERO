ba87f99c84aa9e46246c54313dc8fd829796b1d2
Zuull21 <julio.dahi@sptech.school>
2023-05-17 15:25:48 -0300

    Atualização Site e BD

--- site/src/database/ProjectZero.sql
+++ site/src/database/ProjectZero.sql
@@ -5,9 +5,13 @@ create table Usuario (
 idUsuario int primary key auto_increment,
 Nome Varchar(50),
 Email Varchar(50),
-Senha char(8)
+Senha int,
+fkTierList int,
+constraint fkTierList
+foreign key (fkTierList) 
+references TierList(idTierlist)
 );
-select * from usuario;
+
 create table Tierlist (
 idTierlist int primary key auto_increment,
 Fita1 varchar(45),
@@ -15,11 +19,7 @@ Fita2 varchar(45),
 Fita3 varchar(45),
 Personagem1 varchar(45),
 Personagem2 varchar(45),
-Personagem3 varchar(45),
-fkUsuario int,
-constraint fkUsuario
-foreign key (fkUsuario) 
-references Usuario(idUsuario)
+Personagem3 varchar(45)
 ) auto_increment = 1000;
 
 create table TierListFavorita (
