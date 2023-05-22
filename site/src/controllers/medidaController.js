f9d7b5ddf44155fee5b261dd4a1bbf24bf835aa1
Zuull21 <julio.dahi@sptech.school>
2023-05-22 14:16:44 -0300

    Att TierList

--- site/src/controllers/medidaController.js
+++ site/src/controllers/medidaController.js
@@ -2,13 +2,13 @@ var medidaModel = require("../models/medidaModel");
 
 function buscarUltimasMedidas(req, res) {
 
-    const limite_linhas = 7;
+    const limite_linhas = 12;
 
-    var idAquario = req.params.idAquario;
+    var idTierListFavorita = req.params.idTierListFavorita;
 
     console.log(`Recuperando as ultimas ${limite_linhas} medidas`);
 
-    medidaModel.buscarUltimasMedidas(idAquario, limite_linhas).then(function (resultado) {
+    medidaModel.buscarUltimasMedidas(idTierListFavorita, limite_linhas).then(function (resultado) {
         if (resultado.length > 0) {
             res.status(200).json(resultado);
         } else {
@@ -24,11 +24,11 @@ function buscarUltimasMedidas(req, res) {
 
 function buscarMedidasEmTempoReal(req, res) {
 
-    var idAquario = req.params.idAquario;
+    var idTierListFavorita = req.params.idTierListFavorita;
 
     console.log(`Recuperando medidas em tempo real`);
 
-    medidaModel.buscarMedidasEmTempoReal(idAquario).then(function (resultado) {
+    medidaModel.buscarMedidasEmTempoReal(idTierListFavorita).then(function (resultado) {
         if (resultado.length > 0) {
             res.status(200).json(resultado);
         } else {
