3ff30d89ffdbb76929ee9a3faad090ee3eb42443
Zuull21 <julio.dahi@sptech.school>
2023-05-30 15:00:27 -0300

    Tier funcionando, começo de linkagem quiz

--- Banco de Dados/ProjectZero.sql
+++ Banco de Dados/ProjectZero.sql
@@ -5,28 +5,22 @@ create table Usuario (
 idUsuario int primary key auto_increment,
 Nome Varchar(50),
 Email Varchar(50),
-Senha char(8)
+Senha char(8),
+idQuiz int, constraint fkQuiz foreign key (fkQuiz) references Quiz(idQuiz)
 );
 select * from usuario;
 create table Tierlist (
 idTierlist int primary key auto_increment,
-Fita1 varchar(45),
-Fita2 varchar(45),
-Fita3 varchar(45),
-Personagem1 varchar(45),
-Personagem2 varchar(45),
-Personagem3 varchar(45),
+FitaFav varchar(45),
+PersonagemFav varchar(45),
 fkUsuario int,
 constraint fkUsuario
 foreign key (fkUsuario) 
 references Usuario(idUsuario)
 ) auto_increment = 1000;
 
-create table TierListFavorita (
-idTierListFavorita int primary key auto_increment,
-FitaFav varchar(45),
-PersonagemFav varchar(45),
-fkTierList int,
-constraint FkTL foreign key (fkTierList)
-references Tierlist(idTierList)
+create table Quiz (
+idQuiz int primary key auto_increment,
+QtdAcertosPorcent varchar(45),
+QtdErrosPorcent varchar(45)
 );
